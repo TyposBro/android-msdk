df72b79457741829bf9fd8d7719c0110e789c620
typosbro <typosbro@proton.me>
2025-08-10 23:32:45 +0900

    small updt

--- mobilesdk/src/main/java/uz/click/mobilesdk/core/data/InitialResponse.kt
+++ mobilesdk/src/main/java/uz/click/mobilesdk/core/data/InitialResponse.kt
@@ -4,6 +4,7 @@ import com.squareup.moshi.Json
 
 /**
  * @author rahmatkhujaevs on 29/01/19
+ * @constructor typosbro on 25/08/25
  * */
 data class InitialResponse(
     @field:Json(name = "error_code")
